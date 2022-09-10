dba62f2ea762ce80a041522e837f5ce3f21f592e
Thomas King <102960825+tkgnm@users.noreply.github.com>
2022-09-10 19:08:19 +0100

    settings view now changing depending on preferences! woohoo !

--- TechniumRandomApp/TechniumRandom/NotificationsManager.swift
+++ TechniumRandomApp/TechniumRandom/NotificationsManager.swift
@@ -10,73 +10,79 @@ import UserNotifications
 
 class NotificationsManager: ObservableObject {
 
-// MARK: Notification settings
+    // MARK: Notification settings
 
     enum TimeUnit: String {
         case daily, weekly
     }
 
-    let timeUnits: [TimeUnit] = [.daily, .weekly]
+    let timeUnits: [TimeUnit]
 
     enum DayOfWeek: String {
         case monday, tuesday, wednesday, thursday, friday, saturday, sunday
     }
 
-    let daysOfWeek: [DayOfWeek] = [.monday, .tuesday, .wednesday, .thursday, .friday, .saturday, .sunday]
+    let daysOfWeek: [DayOfWeek]
+    let center: UNUserNotificationCenter
 
-    @Published var notificationsEnabled = false
-    @Published var frequency: TimeUnit = .daily
-    @Published var dayOfWeek: DayOfWeek = .monday
-    @Published var notificationTime = Calendar.current.date(bySettingHour: 10, minute: 30, second: 0, of: Date()) ?? .now
+    @Published var notificationsDisabled: Bool
+    @Published var frequency: TimeUnit
+    @Published var dayOfWeek: DayOfWeek
+    @Published var notificationTime: Date
 
-// MARK: Notification center
+    init() {
 
-    let center = UNUserNotificationCenter.current()
+        self.frequency = .daily
+        self.dayOfWeek = .monday
+        self.notificationTime = Calendar.current.date(bySettingHour: 10, minute: 30, second: 0, of: Date()) ?? .now
+        self.timeUnits = [.daily, .weekly]
+        self.daysOfWeek = [.monday, .tuesday, .wednesday, .thursday, .friday, .saturday, .sunday]
+        self.center = UNUserNotificationCenter.current()
+        self.notificationsDisabled = false
 
-    func requestAuthorization() {
+        center.getNotificationSettings { settings in
 
-        if notificationsEnabled == false {
-            center.getNotificationSettings { settings in
-
+            switch settings.authorizationStatus {
+                case .denied:
+                    self.notificationsDisabled = true
+                default:
+                    self.requestAuthorisation()
+//                    self.notificationsDisabled = false
             }
-        }
-        center.requestAuthorization(options: [.alert, .sound, .badge]) { success, error  in
-            if success {
-                print ("Notification request granted")
-//                the below needs to be replaced with a string of advice from some sort of AdviceManager
-                self.addNotification(for: "my advice")
-            } else if let error = error {
-                print(error.localizedDescription)
-            }
-          // TODO: Fetch notification settings
         }
     }
 
-    func addNotification(for advice: String) {
-
-        let addRequest = {
-            let content = UNMutableNotificationContent()
-            content.title = "Some advie for you today"
-            content.subtitle = "Live long and prosper"
-            content.sound = UNNotificationSound.default
-
-            var dateComponents = DateComponents()
-            dateComponents.hour = 9
-//            let trigger = UNCalendarNotificationTrigger(dateMatching: dateComponents, repeats: false)
-
-            let trigger = UNTimeIntervalNotificationTrigger(timeInterval: 5, repeats: false)
-
-
-            let request = UNNotificationRequest(identifier: UUID().uuidString, content: content, trigger: trigger)
-            self.center.add(request)
-        }
-
+    func checkAuthorisationStatus()  {
         center.getNotificationSettings { settings in
-            if settings.authorizationStatus == .authorized {
-                addRequest()
-            } else {
-                self.requestAuthorization()
+            switch settings.authorizationStatus {
+                case .denied:
+                    self.notificationsDisabled = true
+                default:
+                    self.requestAuthorisation()
+//                    self.notificationsDisabled = false
             }
         }
+    }
+
+    // MARK: Notification center
+
+
+    func requestAuthorisation() {
+        center.requestAuthorization(options: [.alert, .badge, .sound]) { granted, error in
+
+            if let error = error {
+                print (error.localizedDescription)
+            }
+            if granted {
+                self.notificationsDisabled = false
+            } else {
+                self.notificationsDisabled = true
+            }
+        }
+    }
+
+    func cancelNotifications() {
+//        code to come
+        print("cancel notifications called")
     }
 }
