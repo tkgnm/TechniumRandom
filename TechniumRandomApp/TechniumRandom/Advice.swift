637371257561ebca75b40908673d56d61e370c38
Thomas King <102960825+tkgnm@users.noreply.github.com>
2022-11-07 22:34:26 +0000

    now working with history

---
+++ TechniumRandomApp/TechniumRandom/Advice.swift
@@ -0,0 +1,13 @@
+//
+//  Advice.swift
+//  TechniumRandom
+//
+//  Created by Thomas King on 07/11/2022.
+//
+
+import Foundation
+
+struct Advice: Codable {
+    let advice: String
+    var dateRead: Date?
+}
